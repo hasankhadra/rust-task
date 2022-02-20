a2e705c1b36caec848fab1544806509537a875c0
hasankhadra <hasankhadra2013@gmail.com>
2022-02-20 22:30:02 +0300

    fixing bugs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod function;
 
 fn main() {
     let vector: Vec<i32> =  (0..10).collect();
-    let output = function::split_work(vector, f);
+    let output = function::split_work(&vector, f);
     for x in output.iter() {
         println!("> {}", x);
     }
